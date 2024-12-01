f0745d257b5bd58a811726bf3d9015e25a94842b
Ivan Cortes <ivancb97@gmail.com>
2024-12-01 14:40:44 -0300

    modified:   src/components/NavBar/CartWidget.jsx 	modified:   src/components/NavBar/NavBar.jsx

--- src/components/NavBar/CartWidget.jsx
+++ src/components/NavBar/CartWidget.jsx
@@ -3,12 +3,12 @@ import { IoCartOutline } from "react-icons/io5";
 const CartWidget = ({ menuOpen }) => {
   return (
     <div
-      className={`p-2 items-center w-fit flex flex-col ${
-        menuOpen ? "block" : "hidden"
+      className={`relative flex-col items-center p-2 w-fit  ${
+        menuOpen ? "flex" : "hidden"
       } lg:flex`}
     >
       <IoCartOutline size={40} color="white" />
-      <span>1</span>
+      <div className="bg-red-500 rounded-full w-7 h-7 flex justify-center items-center absolute top-0 right-0">1</div>
     </div>
   );
 };
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import CartWidget from "./CartWidget";
 import HamburgerMenu from "./HamburgerMenu";
-import { GiPumpkinMask } from "react-icons/gi";
+import {GiPumpkinMask} from "react-icons/gi";
 import {useState, useEffect} from "react";
 
 const NavBar = () => {
@@ -26,20 +26,17 @@ const NavBar = () => {
 
     return (
         <nav className="bg-slate-900 text-gray-200 h-auto p-4 lg:flex lg:justify-between">
-            <div className="p-2 lg:w-auto flex justify-center items-center">
-                <GiPumpkinMask
-                    color="orange"
-                    size={50}
-                 />
+            <div className="flex justify-center items-center p-2 w-auto">
+                <GiPumpkinMask color="orange" size={50} />
                 <h1 className="font-sans font-semibold text-xl tracking-wider text-center p-3 select-none">
                     MICHAEL BUYERS
                 </h1>
             </div>
             <HamburgerMenu menuOpen={menuOpen} toggleMenu={toggleMenu} />
             <ul
-                className={`lg:flex lg:flex-row lg:items-center lg:flex-grow ${
+                className={`${
                     menuOpen ? "flex" : "hidden"
-                } flex-col`}
+                } flex-col lg:flex lg:flex-row lg:items-center lg:flex-grow `}
             >
                 {items.map((item) => (
                     <li
