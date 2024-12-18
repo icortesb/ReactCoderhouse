ad29c3cdd25cea5854b61a5fd4d6064295594c95
Ivan Cortes <ivancb97@gmail.com>
2024-12-18 14:29:58 -0300

    modified:   src/components/ItemListContainer/ItemList.jsx

--- src/components/ItemListContainer/ItemList.jsx
+++ src/components/ItemListContainer/ItemList.jsx
@@ -2,7 +2,7 @@ import Item from "./Item"; // Import the Item component
 
 const ItemList = ({ products }) => {
     return (
-        <div className="flex flex-wrap gap-5 p-2 md:p-5">
+        <div className="flex flex-wrap gap-5 p-12 md:p-5">
             {products.map((product) => (
                 <Item key={product.id} product={product} />
             ))}
