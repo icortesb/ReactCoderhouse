23f68518a8da5cecec360763aab1b1854fd5995e
Ivan Cortes <ivancb97@gmail.com>
2024-12-19 16:12:02 -0300

    Add useNavigate for error redirect to /

--- src/components/ErrorRedirect/ErrorRedirect.jsx
+++ src/components/ErrorRedirect/ErrorRedirect.jsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import ErrorToast from "../ErrorToast/ErrorToast";
+import { useNavigate } from "react-router-dom";
 
 const ErrorRedirect = () => {
+    const navigate = useNavigate();
     useEffect(() => {
         const timer = setTimeout(() => {
-            window.location.replace("/");
+            navigate("/");
         }, 1500);
         return () => clearTimeout(timer);
     }, []);
