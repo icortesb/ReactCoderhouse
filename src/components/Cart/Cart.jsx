af0fbd4b5718e86cfbe3cd0883a96198e7c13c38
Ivan Cortes <ivancb97@gmail.com>
2024-12-25 20:40:13 -0300

    modified:   src/components/Cart/Cart.jsx

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,8 +5,10 @@ import { CartContext } from "../../context/CartContext"
 const Cart = () => {
     const { cart, removeProduct } = useContext(CartContext)
 
+    const totalPrice = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+
     return (
-        <div>
+        <div className="p-4 bg-gray-100 min-h-screen">
             {cart.length === 0 ? (
                 <div className="flex flex-col items-center justify-center h-96">
                     <h1 className="text-2xl mb-4">No hay productos en el carrito</h1>
@@ -15,11 +17,11 @@ const Cart = () => {
                     </Link>
                 </div>
             ) : (
-                <div className="p-4">
+                <div className="p-4 bg-white rounded-lg shadow-md">
                     <ul>
                         {cart.map((item) => (
                             <li key={item.id} className="flex items-center border-b border-gray-300 py-4">
-                                <img src={item.imgPreview} alt={item.name} className="w-20 h-20 object-cover mr-4" />
+                                <img src={item.imgPreview} alt={item.name} className="w-20 h-20 object-cover mr-4 rounded-lg" />
                                 <div className="flex-1">
                                     <h3 className="text-lg font-semibold">{item.name}</h3>
                                     <p className="text-gray-600">Cantidad: {item.quantity}</p>
@@ -34,6 +36,9 @@ const Cart = () => {
                             </li>
                         ))}
                     </ul>
+                    <div className="mt-4 text-right">
+                        <p className="text-lg font-semibold">Total Price: ${totalPrice}</p>
+                    </div>
                     <button className="mt-4 bg-gradient-to-r from-blue-500 to-purple-500 text-white py-2 px-4 rounded-full shadow-lg transform transition-transform hover:scale-105 hover:from-blue-600 hover:to-purple-600">
                         Finish Purchase
                     </button>
