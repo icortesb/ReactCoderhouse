bd8e053538d6088f52a324f657c3b5daaf3d6cd7
Ivan Cortes <ivancb97@gmail.com>
2025-01-22 21:32:43 -0300

    modified:   src/components/ItemListContainer/Item.jsx

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,34 +1,39 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import React from "react";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
-const Item = ({ product }) => {
-    const [isHovered, setIsHovered] = useState(false);
+const Item = React.memo(({product}) => {
+  const [isHovered, setIsHovered] = useState(false);
 
-    return (
-        <Link 
-            to={`/detail/${product.id}`} 
-            className="border border-gray-300 rounded-lg p-4 w-52 h-96 overflow-hidden flex flex-col"
-            onMouseEnter={() => {
-                setIsHovered(true);
-            }}
-            onMouseLeave={() => {
-                setIsHovered(false);
-            }}
-            title={product.name}
-        >
-            <img 
-                src={isHovered ? product.imgDetail : product.imgPreview} 
-                alt={product.name} 
-                className="w-48 h-48 object-cover rounded-t-lg"
-            />
-            <div className="p-2 flex-grow">
-                <h2 className="text-xl mb-2 truncate">{product.name}</h2>
-                <p className="mb-2 truncate">{product.description}</p>
-                <p className="font-bold mb-2">${product.price}</p>
-                <p className="text-gray-600">Stock: {product.stock}</p>
-            </div>
-        </Link>
-    );
-};
+  console.log(`Renderizando ${product.name}`);
+
+  return (
+    <Link
+      to={`/detail/${product.id}`}
+      className="flex flex-col p-4 overflow-hidden border border-gray-300 rounded-lg w-52 h-96"
+      onMouseEnter={() => {
+        setIsHovered(true);
+      }}
+      onMouseLeave={() => {
+        setIsHovered(false);
+      }}
+      title={product.name}
+    >
+      <img
+        src={isHovered ? product.imgDetail : product.imgPreview}
+        alt={product.name}
+        className="object-cover w-48 h-48 rounded-t-lg"
+      />
+      <div className="flex-grow p-2">
+        <h2 className="mb-2 text-xl truncate">{product.name}</h2>
+        <p className="mb-2 truncate">{product.description}</p>
+        <p className="mb-2 font-bold">${product.price}</p>
+        <p className="text-gray-600">Stock: {product.stock}</p>
+      </div>
+    </Link>
+  );
+}, (prevProps, nextProps) => {
+  return prevProps.product.id === nextProps.product.id;
+});
 
 export default Item;
